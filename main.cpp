e3c80c816356633864819e79b3c324ed2f92cb9c
A9223412 <79957469+A9223412@users.noreply.github.com>
2021-06-18 18:00:32 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include"Poker.h"
+int main()
+{
+    int choose = 1;
+    Poker poker;
+    cout << "****************** 歡迎玩二十一點遊戲 ******************" << endl << endl;
+    poker.inputBet();
+    poker.initPoker();  //洗牌
+
+    while (choose == 1 || choose == 2 || choose == 3 || choose == 4)
+    {
+        cout << "1.要牌 2.不要牌 3.重新開始 4.退出  >>請輸入數字選擇操作:";
+        cin >> choose;
+        if (choose == 1)            poker.farmerAsk();
+        else if (choose == 2)      poker.landlordProcess();
+        else if (choose == 3)      poker.newGame();
+        else if (choose == 4)      exit(0);
+    }
+
+    return 0;
+}
